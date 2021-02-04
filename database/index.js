26f77d05802c1d28eca0a76b0f5beeb6d2b11f40
Minh Tran <mctran98@yahoo.com>
2021-02-03 17:13:18 -0800

    Query to add table

--- database/index.js
+++ database/index.js
@@ -8,9 +8,9 @@ const pool = new Pool({
 });
 
 const createTable = `
-CREATE TABLE IF NOT EXISTS user (user_id SERIAL PRIMARY KEY, user_name VARCHAR (16), account_name VARCHAR (16), user_location VARCHAR (64), user_rating INT);
-CREATE TABLE IF NOT EXISTS product (prod_id SERIAL PRIMARY KEY, prod_desc VARCHAR (256), prod_name VARCHAR (16), prod_price INT, prod_sales INT, prod_discount INT, prod_stocks INT, user_id FOREIGN KEY, ship_id FOREIGN KEY);
-CREATE TABLE IF NOT EXISTS shipping (ship_id SERIAL PRIMARY KEY, ship_time INT, delivery_time INT, prod_info VARHAR (64));
+CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY, user_name VARCHAR (16), account_name VARCHAR (16), user_location VARCHAR (64), user_rating INT);
+CREATE TABLE IF NOT EXISTS shippings (ship_id SERIAL PRIMARY KEY, ship_time INT, delivery_time INT, prod_info VARCHAR (64));
+CREATE TABLE IF NOT EXISTS products (prod_id SERIAL PRIMARY KEY, prod_desc VARCHAR (256), prod_name VARCHAR (16), prod_price INT, prod_sales INT, prod_discount INT, prod_stocks INT, user_id INT REFERENCES users (user_id), ship_id INT REFERENCES shippings (ship_id));
 `;
 
 pool.query(createTable, (err, res) => {
