a2d7408521ad713d96dc72ee3f3c474689fb65a1
Minh Tran <mctran98@yahoo.com>
2021-02-04 17:34:30 -0800

    Implemented data generation

--- database/index.js
+++ database/index.js
@@ -8,14 +8,13 @@ const pool = new Pool({
 });
 
 const createTable = `
-CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY, user_name VARCHAR (16), account_name VARCHAR (16), user_location VARCHAR (64), user_rating INT);
-CREATE TABLE IF NOT EXISTS shippings (ship_id SERIAL PRIMARY KEY, ship_time INT, delivery_time INT, prod_info VARCHAR (64));
-CREATE TABLE IF NOT EXISTS products (prod_id SERIAL PRIMARY KEY, prod_desc VARCHAR (256), prod_name VARCHAR (16), prod_price INT, prod_sales INT, prod_discount INT, prod_stocks INT, user_id INT REFERENCES users (user_id), ship_id INT REFERENCES shippings (ship_id));
+CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY, user_name VARCHAR (64), account_name VARCHAR (64), user_location VARCHAR (16), user_rating NUMERIC(2,1));
+CREATE TABLE IF NOT EXISTS shippings (ship_id SERIAL PRIMARY KEY, ship_time VARCHAR(16), delivery_time VARCHAR(16), prod_info VARCHAR (128));
+CREATE TABLE IF NOT EXISTS products (prod_id SERIAL PRIMARY KEY, prod_desc VARCHAR (256), prod_name VARCHAR (64), prod_price VARCHAR(16), prod_sales INT, prod_discount NUMERIC(2,1), prod_stocks INT);
 `;
 
 pool.query(createTable, (err, res) => {
   if (err) console.error(err);
-  pool.end();
 });
 
 module.exports = pool;
--- database/seed.js
+++ database/seed.js
@@ -1,1 +1,33 @@
 const pool = require('./index.js');
+const path = require('path');
+const fs = require('fs');
+const faker = require('faker');
+const dayjs = require('dayjs');
+
+for (let i = 0; i < 4; i++) {
+  let users = '', products = '', shippings = '';
+  for (let j = 0; j < 2500000; j++) {
+    products += `"${faker.commerce.productDescription()}","${faker.commerce.productName()}","$${faker.commerce.price()}",${faker.random.number({min: 0, max: 1000})},${faker.random.float({min: 0, max: 0.5})},${faker.random.number({min: 1, max: 100})}\n`;
+
+    users += `"${faker.name.findName()}","${faker.internet.userName()}","${faker.address.stateAbbr()}",${faker.random.float({min:0, max: 5})}\n`;
+
+    shippings += `"${dayjs(faker.date.future()).format('MMM D')}","${dayjs(faker.date.soon()).format('h:mm A')}","${faker.commerce.productMaterial()}"\n`;
+  }
+  fs.writeFile('products.csv', products, {'flag': 'a'}, err => {
+    if (err) console.error(err);
+  });
+  fs.writeFile('users.csv', users, {'flag': 'a'}, err => {
+    if (err) console.error(err);
+  });
+  fs.writeFile('shippings.csv', shippings, {'flag': 'a'}, err => {
+    if (err) console.error(err);
+  });
+}
+
+console.log('Data Generation Done');
+
+pool.query(`COPY products(prod_desc, prod_name, prod_price, prod_sales, prod_discount, prod_stocks) FROM '${path.join(__dirname, '..', 'products.csv')}' DELIMITER ',' CSV`);
+
+pool.query(`COPY users(user_name, account_name, user_location, user_rating) FROM '${path.join(__dirname, '..', 'users.csv')}' DELIMITER ',' CSV`);
+
+pool.query(`COPY shippings(ship_time, delivery_time, prod_info) FROM '${path.join(__dirname, '..', 'shippings.csv')}' DELIMITER ',' CSV`);
