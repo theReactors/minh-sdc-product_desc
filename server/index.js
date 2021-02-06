9e0965dcc99725a18c8b6a11e28aacd2c43a4100
Minh Tran <mctran98@yahoo.com>
2021-02-05 17:32:04 -0800

    Installed some packages & implemented simple query

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const path = require('path');
+const pool = require('../database/index.js');
+const bodyParser = require('body-parser');
+const compression = require('compression');
 const app = express();
 const port = 3000;
 
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.send('Hello World');
+});
+
+app.get('description/:id', (req, res) => {
+  pool.query('SELECT prod_name FROM products WHERE prod_id = 1337', (err, data) => {
+    if (err) console.error(err);
+    res.send(data.rows);
+  });
 });
 
 app.listen(port, () => {
